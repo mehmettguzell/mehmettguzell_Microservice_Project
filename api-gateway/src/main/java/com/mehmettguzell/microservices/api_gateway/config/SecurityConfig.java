46a94705425d614a77e4681c47973eccb8f1411f
mehmettguzell <>
2025-09-04 20:39:36 +0300

    Refactor API Gateway, temporarily disable Keycloak, move server URLs to application.yml

--- api-gateway/src/main/java/com/mehmettguzell/microservices/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/mehmettguzell/microservices/api_gateway/config/SecurityConfig.java
@@ -1,34 +1,49 @@
-package com.mehmettguzell.microservices.api_gateway.config;
+package com.mehmettguzell.microservices.api_gateway.config;//package com.mehmettguzell.microservices.api_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
+
+// Authorization is off
 @Configuration
 public class SecurityConfig {
-
-    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/aggregate/**", "/api-docs/**"};
-
     @Bean
-    public SecurityFilterChain SecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(auth -> auth
-                        .requestMatchers(freeResourceUrls).permitAll()
-                        .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-        return httpSecurity.build();
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(ex -> ex.anyExchange().permitAll())
+                .csrf(csrf -> csrf.disable());
+        return http.build();
     }
-
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("*"));
-//        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-//        configuration.setAllowedHeaders(List.of("*"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
 }
 
+
+//@EnableWebFluxSecurity
+//@Configuration
+//public class SecurityConfig {
+//
+//    private final String[] freeResourceUrls = {
+//            "/swagger-ui.html",
+//            "/swagger-ui/**",
+//            "/v3/api-docs/**",
+//            "/swagger-resources/**",
+//            "/aggregate/**",
+//            "/api-docs/**"
+//    };
+//
+//    @Bean
+//    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+//        http
+//                .authorizeExchange(auth -> auth
+//                        .pathMatchers(freeResourceUrls).permitAll()
+//                        .anyExchange().authenticated()
+//                )
+//                .oauth2ResourceServer(oauth2 -> oauth2
+//                        .jwt(Customizer.withDefaults())
+//                );
+//
+//        return http.build();
+//    }
