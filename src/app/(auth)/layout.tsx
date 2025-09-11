e12f10842460f48f88f4aa81e295e75ec65c1d9a
mehmettguzell <>
2025-09-11 17:44:58 +0300

    Link

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,21 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+  { href: "/login", label: "Login" },
+  { href: "/register", label: "Register" },
+];
+
 export default function authLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  const pathname = usePathname();
+
   return (
     <html lang="en">
       <body>
@@ -10,6 +23,21 @@ export default function authLayout({
         <header style={{backgroundColor: 'lightgray', padding: '1rem'}}>
           <p>Auth Header</p>
         </header>
+
+          <div>
+              {navLinks.map((link) => {
+                const isActive = pathname === link.href || (link.href !== '/');
+                return (
+                  <Link
+                    className={isActive ? 'font-bold mr-4' : 'text-gray-500 mr-4'}
+                    href={link.href}
+                    key={link.href}
+                    >
+                    {link.label}
+                    </Link>
+                );
+              })}
+          </div>
         
         {children}
 
--- src/app/(pages)/product/page.tsx
+++ src/app/(pages)/product/page.tsx
@@ -1,3 +1,23 @@
+import Link from "next/link";
+
 export default function Product() {
-    return <h1>Product Page</h1>;
+    const productId = 100;
+    return (
+        <>
+            <h1>Product Page</h1>
+            <Link href="/">Home</Link>
+            <h2>
+                <Link href="/product/1">Product 1</Link>
+            </h2>
+            <h2>
+                <Link href="/product/2">Product 2</Link>
+            </h2>
+            <h2>
+                <Link href="/product/3" replace>Product 3</Link>
+            </h2>
+            <h2>
+                <Link href={`/product/${productId}`}>Product {productId}</Link>
+            </h2>
+        </>
+    );
 }
