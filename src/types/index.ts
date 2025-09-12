a99b0d2ed804e1cab6bb23d2d70df8751801399f
mehmettguzell <>
2025-09-12 14:53:40 +0300

    Project skeleton created

---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+export type Prodcut = {
+    id: string;
+    name: string;
+    skuCode: string;
+    description?: string;
+    price: number;
+}
+
+export type Inventory = {
+    id: string;
+    skuCode: string;
+    quantity: number;
+}
+
+export type OrderStatus = 'PENDING' | 'COMPLETED' | 'CANCELLED';
+
+export type OrderRequest = {
+  orderNumber: string;
+  skuCode: string;
+  price: string;
+  quantity: number;
+};
+
+export type OrderResponse = {
+  id: number;
+  orderNumber: string;
+  skuCode: string;
+  price: string;
+  quantity: number;
+  status: OrderStatus;
+};
