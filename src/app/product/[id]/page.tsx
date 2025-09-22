2938a1503b8fd75dc81c53081189de74c6b02173
mehmettguzell <>
2025-09-23 02:00:32 +0300

    bug fixed

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -11,7 +11,7 @@ interface Props {
 }
 
 export default async function ProductIdPage({ params }: Props) {
-  const { id: productId } = params;
+  const { id: productId } = await params;
 
   let product: Product | ApiErrorData;
   try {
